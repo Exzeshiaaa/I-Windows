657e514bff336a563181a0e2e2ff66a430a64c53
Adrian <phyxiaperez@gmail.com>
2024-05-23 00:32:07 +0800

    Initial Commmit

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+    
+</script>
+
+<class class="index">
+    <h2>
+        This is my title
+    </h2>
+    
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae incidunt ipsum rerum quasi facere perferendis recusandae delectus a repellendus? Earum temporibus libero quas repellat numquam unde eius molestiae dolorem iusto, reiciendis vitae aliquam. Vel hic reiciendis unde aspernatur recusandae quibusdam? Placeat excepturi architecto dolor beatae delectus corporis perspiciatis repellat repudiandae.</p>
+</class>
+
+
+<style>
+    .index {
+        text-align: center;
+    }
+</style>
