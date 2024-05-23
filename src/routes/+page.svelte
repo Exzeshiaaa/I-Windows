c18eda617f88c3c8bf0cb163e4f33037420a38f5
Adrian <phyxiaperez@gmail.com>
2024-05-23 16:40:11 +0800

    Added Sign In Page and Additional CSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+
+<body class="IndexBody">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <button class = LogInButton><a href = "/LogIn">Go to Sign In</a></button>
+</body>
+    
+
+
+
+
+<style>   
+    
+</style>
---
+++ src/routes/LogIn/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+    let Title = "I-WINDOWS"
+
+</script>
+
+<body class="SignBody">
+    <div>
+        <h2 class="title">{Title}</h2>
+    </div>
+    
+    <div class="Sign">
+        <p class="SignHead"> Welcome Back </p>
+        <p class=SubText> Enter your credentials to continue.</p>
+    </div>  
+</body>
+
+
+<style>
+    .Sign {
+        position: relative;
+        display: block;
+        justify-content: center;
+        border-radius: 65px;
+        width: 57%;
+        height: 60vh;
+        align-items: center;
+        margin: auto;
+        background: linear-gradient(white, white) padding-box, linear-gradient(90deg, #48d0b8, #9889f9) border-box;
+        border: 6px solid transparent;
+    }
+
+    .SignHead {
+        color: #1e1e1e;
+        font-size: 60px;
+        position: relative;
+        text-align: center;
+        text-shadow: 0px 3px 2.5px #333333;
+        margin: 12px 0 0 2px;
+        padding: 0 0 0 0;
+    }
+
+    .SubText {
+        font-family: 'Poppins';
+        text-align: center;
+        padding: 0 0 0 0;
+        margin: -8px 0 0 0;
+        font-size: 17px;
+    }
+
+</style>
