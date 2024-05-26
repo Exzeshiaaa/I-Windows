c3af2647dee6518741183223d764428aad60d51e
fqbueno <fqbueno@up.edu.ph>
2024-05-27 02:56:34 +0800

    edited Login for mobile

--- src/routes/LogIn/+page.svelte
+++ src/routes/LogIn/+page.svelte
@@ -6,58 +6,87 @@
 </script>
 
 <body>
-    <div class="headContainer">
-        <h2 class="title">{Title}</h2>
-    </div>
-
-    <br><br><br>
-
-    <div class="bodyContainer">
-        <div class="backDrop">
-            <div class="dropHead">
-                <p class="signHead"> Welcome Back </p>
-                <p class=subText> Enter your credentials to continue.</p>
-            </div>
-
-            <div class="userCred">
-                <label class="txtLabel" for="userName">Username: </label>
-                <input class="inpBox" type="text" id="userName" placeholder="Enter your username here" bind:value={userName}>
-            </div>
-
-            <div class="passCred">
-                <label class="txtLabel" for="userName">Password: </label>
-                <input class="inpBox" type="password" id="password" placeholder="Enter your password here" bind:value={password}>
-            </div>
-
-            <div class="submitCont">
-                <a href="/Home"><button class="submitBtn" type="button">Log In</button></a>
-            </div>
-
-            <div class="regCont">
-                <span>Don't have an account?</span>
-                <span class="regText">Register here</span>
-            </div>
-
-            <hr class="end">
+    <div class="siteWrapper">
+        <div class="headContainer">
+            <h2 class="title">{Title}</h2>
         </div>
-
+    
+        <!-- <br><br><br> -->
+    
+        <div class="bodyContainer">
+            <div class="backDrop">
+                <div class="dropHead">
+                    <p class="signHead"> Welcome Back </p>
+                    <p class=subText> Enter your credentials to continue.</p>
+                </div>
+    
+                <div class="userCred">
+                    <label class="txtLabel" for="userName">Username: </label>
+                    <input class="inpBox" type="text" id="userName" placeholder="Enter your username here" bind:value={userName}>
+                </div>
+    
+                <div class="passCred">
+                    <label class="txtLabel" for="userName">Password: </label>
+                    <input class="inpBox" type="password" id="password" placeholder="Enter your password here" bind:value={password}>
+                </div>
+    
+                <div class="submitCont">
+                    <a href="/Home"><button class="submitBtn" type="button">Log In</button></a>
+                </div>
+    
+                <div class="regCont">
+                    <span class="regText1">Don't have an account?</span>
+                    <span class="regText2">Register here</span>
+                </div>
+    
+                <hr class="end">
+            </div>
+    
+        </div>
     </div>
+
 </body>
 
 <svelte:head>
     <style>
-        html {
+        html, body{
             max-width: 100%;
-            min-height: 100%;
+            max-height: 100%;
             background-color: #212429;
+            
         }
+
+        @media only screen and (max-width: 600px){
+        html {
+            overflow:hidden;
+            transform:scale(1.3);
+        }   
+    }
+
     </style>
 </svelte:head>
 
 <style>
+
+    .headContainer {
+        align-self: center;
+        max-width: 40%;
+        max-height: 100px;
+        margin: auto;
+        margin-bottom: 2.88%;
+    }
+
     .bodyContainer {
         margin: auto;
+        height: 632px;
+        max-width: 100%;
     }
+
+    @media only screen and (max-width: 600px){
+        .bodyContainer{height: 160px;}
+        .headContainer{margin-top: 190px;}
+    }
+
 
     .backDrop {
         position: relative;
@@ -65,7 +94,7 @@
         justify-content: center;
         border-radius: 60px;
         width: 60%;
-        height: 620px;
+        height: 98.10%;
         align-items: center;
         margin: auto;
         background: linear-gradient(white, white) padding-box, linear-gradient(90deg, #48d0b8, #9889f9) border-box;
@@ -74,7 +103,7 @@
 
     .signHead {
         color: #1e1e1e;
-        font-size: 350%;
+        font-size: 2.915vw;
         position: relative;
         text-align: center;
         text-shadow: 0px 3px 2.5px #333333;
@@ -86,13 +115,13 @@
         font-family: 'Poppins';
         text-align: center;
         padding: 0 0 0 0;
-        margin: -8px 0 0 0;
-        font-size: 17px;
+        margin: -0.7% 0 0 0;
+        font-size: 0.885vw;
     }
 
     .userCred {
-        width: auto;
-        height: auto;
+        width: 100%;
+        height: 6.3%;
         margin-top: 8%;
         margin-bottom: 2%;
         text-align: center;
@@ -101,19 +130,19 @@
     .txtLabel {
         color: #1e1e1e;
         font-family: 'Poppins';
-        font-size: 26px;
+        font-size: 1.35vw;
         position: relative;
         margin: 3px 20px 3px 3px;
         top: 4px;
     }
 
     .inpBox {
-        width: 530px;
-        height: 25px;
+        width: 47.2%;
+        height: 64%;
         border-radius: 5px;
         font-family: 'Poppins';
         font-weight: bold;
-        font-size: 15px;
+        font-size: 0.78vw;
         padding-left: 10px;
         color: #1e1e1e;
     }
@@ -125,18 +154,18 @@
     }
 
     .passCred {
-        width: auto;
-        height: auto;
+        width: 100%;
+        height: 6.3%;
         text-align: center;
-        margin-left: 0.79%;
+        margin-left: 0.36%;
         margin-bottom: 10%;
     }
 
     .submitCont {
-        width: auto;
-        height: auto;
+        width: 100%;
+        height: 9.67%;
         text-align: center;
-        margin-bottom: 18px;
+        margin-bottom: 1.58%;
     }
 
     .submitBtn {
@@ -145,10 +174,10 @@
         color: #F1F1F1;
         font-family: 'Poppins', monospace;
         text-align: center;
-        font-size: 36px;
-        size: 30px;
-        width: 200px;
-        height: 60px;
+        font-size: 1.88vw;
+        /* size: 30px; */
+        width: 17.81%;
+        height: 100%;
         border-radius: 14px;
         cursor: pointer;
         text-shadow: 3px 3px 5px #333333;
@@ -164,29 +193,35 @@
         cursor: pointer;
         font-family: 'Poppins', monospace;
         text-align: center;
-        font-size: 36px;
+        /* font-size: 36px;
         size: 30px;
         width: 200px;
-        height: 60px;
+        height: 60px; */
 
     }
 
     .regCont {
         font-family: 'Poppins';
         text-align: center;
-        width: auto;
-        height: auto;
-        margin-bottom: 70px;
+        width: 100%;
+        height: 4.03%;
+        margin-bottom: 6.2%;
     }
 
-    .regText {
+    .regText1{
+        font-family: 'Poppins';
+        font-size: 0.84vw;
+    }
+
+    .regText2 {
         font-weight: bold;
         color: #0f4253;
+        font-size: 0.84vw;
         text-decoration: underline;
     }
 
     .end {
-        width: 750px;
+        width: 66.78%;
         border-color: #1e1e1e;
         border-width: 1px;
     }
